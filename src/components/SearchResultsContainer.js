324a920ba9686f51768b2ded5eeb91760840d361
Hannah Fernandes-Martin <hfm11295@gmail.com>
2021-05-13 20:43:48 -0400

    created API.js and basic components files

---
+++ src/components/SearchResultsContainer.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import SearchForm from "./SearchForm";
+import ResultList from "./ResultList";
+import API from "../utils/API";
+
+class SearchResultsContainer extends Component {
+
+}
+
+export default SearchResultContainer;
---
+++ src/utils/API.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const BASEURL = "https://randomuser.me/api/"
+
+export default {
+    getEmployees: function (name){
+        return axios.get(BASEURL + "?results=1000");
+    },
+    getEmployeeByName: function (name){
+        return axios.get(BASEURL + "?name=" + name);
+    }
+    // TODO: Figure out how to sort
+}
