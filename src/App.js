074499bb4e8e988ca5fe5af43f8195767c9e45ff
Hannah Fernandes-Martin <hfm11295@gmail.com>
2021-05-23 19:19:17 -0400

    added header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import SearchResultContainer from "./components/SearchResultsContainer";
+import Header from "./components/Header"
 
 function App() {
-  return <SearchResultContainer />;
+  return (
+    <div className="app">
+      <Header />
+      <SearchResultContainer/>
+    </div>
+  );
 }
 
 export default App;
